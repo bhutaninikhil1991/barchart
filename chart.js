3aa1c5fa4fb74f15801a6caac7e3ac36df76601c
Sophie Engle <sjengle@cs.usfca.edu>
2020-01-27 11:42:04 -0800

    Adding templates.

---
+++ chart.js
@@ -0,0 +1,13 @@
+
+let updateData = function() {
+  // TODO: implement function
+  return new Map();
+};
+
+let drawBarChart = function() {
+  // TODO: implement function
+};
+
+let updateBarChart = function() {
+  // TODO: optionally implement function
+};
---
+++ count.js
@@ -0,0 +1,12 @@
+
+let letters = "abcdefghijklmnopqrstuvwxyz".split("");
+
+let keepLetters = function(text) {
+  // TODO: implement function
+  return text;
+};
+
+let countLetters = function(input) {
+  // TODO: implement function
+  return new Map();
+};
