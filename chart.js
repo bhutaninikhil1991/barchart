996c271f7b3d10fa805d4514403419c0255cf713
Sophie Engle <sjengle@cs.usfca.edu>
2020-01-28 10:13:18 -0800

    Adding links.

--- chart.js
+++ chart.js
@@ -6,8 +6,20 @@ let updateData = function() {
 
 let drawBarChart = function() {
   // TODO: implement function
+
+  // https://github.com/d3/d3-array/blob/master/README.md
+
+  // https://bl.ocks.org/mbostock/3019563
+  // https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Positions
+
+  // https://github.com/d3/d3-scale#api-reference
+  // https://github.com/d3/d3-scale#band-scales
+
+  // https://bost.ocks.org/mike/selection/
+  // https://bost.ocks.org/mike/join/
 };
 
 let updateBarChart = function() {
   // TODO: optionally implement function
+  // https://github.com/d3/d3-ease
 };
